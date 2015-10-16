43b1121edce8bad7f7178453731d760227a3969a
Jaagup Siil <jaagsii@greeny.cs.tlu.ee>
2015-10-16 10:41:58 +0300

    added edit stuff

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ require_once("../configglobal.php");
 		
 		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
 		
-		$stmt=$mysqli->prepare("SELECT id, user_id, number_plate, color FROM car_plates");
+		$stmt=$mysqli->prepare("SELECT id, user_id, number_plate, color FROM car_plates WHERE deleted IS NULL");
 		$stmt->bind_result($id, $user_id, $number_plate, $color);
 		$stmt->execute();
 		
@@ -22,11 +22,13 @@ require_once("../configglobal.php");
 			$car=new StdClass();
 			$car->id=$id;
 			$car->number_plate=$number_plate;
+			$car->color=$color;
+			$car->user_id=$user_id;
 			
 			//lisame selle massiivi
 			array_push($array, $car);
 			echo"<pre>";
-			var_dump($array);
+			//var_dump($array);
 			echo"</pre>";
 			
 		}
@@ -34,8 +36,24 @@ require_once("../configglobal.php");
 		$stmt->close();
 		$mysqli->close();
 		
+		return $array;
+	}
+	
+	function deleteCar($id_to_be_deleted){
+		
+		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
+		
+		$stmt=$mysqli->prepare("UPDATE car_plates SET deleted=NOW() WHERE id=?");
+		$stmt->bind_param("i", $id_to_be_deleted);
+		
+		if($stmt->execute()){
+			//kui sa edukalt kustutatud
+			header("Location: table.php");
+		}
+		
+		$stmt->close();
+		$mysqli->close();
 	}
 
-getCarData();
 	
 ?>
